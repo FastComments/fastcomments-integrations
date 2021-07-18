aec8e7c5a835ca5bbf8c98709dac68a335864c8b
winrid <winrid@gmail.com>
2021-07-18 16:30:06 -0700

    add testing data locally, check that it gets synched

--- core/js/example/FastCommentsIntegrationCoreTest.js
+++ core/js/example/FastCommentsIntegrationCoreTest.js
@@ -1,4 +1,4 @@
-const { FastCommentsCoreExampleUsage } = require ('./FastCommentsIntegrationCoreExample');
+const {FastCommentsCoreExampleUsage} = require('./FastCommentsIntegrationCoreExample');
 const puppeteer = require('puppeteer');
 const HOST = process.env.FC_HOST || 'https://fastcomments.com';
 
@@ -26,8 +26,66 @@ const HOST = process.env.FC_HOST || 'https://fastcomments.com';
     await page.click('button[type="submit"]');
     await page.waitForSelector('body');
 
+    // TODO add some testing events to the account (add comments, remove some, update some, add some votes, undo those votes).
+
     try {
         // TODO create some testing comments locally.
+
+        myApp.fastComments.commentDB['external-id-0'] = {
+            "urlId": "test",
+            "url": "https://example.com",
+            "pageTitle": "Some test page title",
+            "userId": "some-user",
+            "commenterName": "Commenter name",
+            "commenterEmail": "someone@fctest.com",
+            "comment": "Automation Test 1",
+            "commentHTML": "<b>Automation Test 1</b>",
+            "date": "2021-07-18T22:03:41.275Z",
+            "votes": 1,
+            "votesUp": 1,
+            "votesDown": 0,
+            "verified": true,
+            "verifiedDate": "2021-07-18T22:03:41.275Z",
+            "notificationSentForParent": true,
+            "notificationSentForParentTenant": true,
+            "reviewed": true,
+            "imported": true,
+            "externalId": "external-id-0",
+            "externalParentId": null,
+            "isSpam": false,
+            "avatarSrc": "https://static.fastcomments.com/1605337537848-DSC_0841.JPG",
+            "hasImages": false,
+            "hasLinks": false,
+            "approved": true
+        };
+
+        myApp.fastComments.commentDB['external-id-1'] = {
+            "urlId": "test",
+            "url": "https://example.com",
+            "pageTitle": "Some test page title",
+            "userId": "some-user",
+            "commenterName": "Commenter name",
+            "commenterEmail": "someone@fctest.com",
+            "comment": "Automation Test 2",
+            "commentHTML": "<b>Automation Test 2</b>",
+            "date": "2021-07-18T22:03:41.275Z",
+            "votes": 1,
+            "votesUp": 1,
+            "votesDown": 0,
+            "verified": true,
+            "verifiedDate": "2021-07-18T22:03:41.275Z",
+            "notificationSentForParent": true,
+            "notificationSentForParentTenant": true,
+            "reviewed": true,
+            "imported": true,
+            "externalId": "external-id-1",
+            "externalParentId": "external-id-0",
+            "isSpam": false,
+            "avatarSrc": "https://static.fastcomments.com/1605337537848-DSC_0841.JPG",
+            "hasImages": false,
+            "hasLinks": false,
+            "approved": true
+        };
 
         async function tryToValidateToken() {
             const token = await myApp.fastComments.getSettingValue('fastcomments_token');
@@ -45,13 +103,48 @@ const HOST = process.env.FC_HOST || 'https://fastcomments.com';
             tryToValidateToken(), // have the admin user visit the page to setup the integration
         ]);
 
-        // TODO sync our local testing data (comments) to the FastComments backend.
+        // send our local testing data (comments) to the FastComments backend.
+        async function syncToFastComments() {
+            return new Promise((resolve) => {
+                async function tick() {
+                    await myApp.cron();
+                    const isSyncDone = await page.$eval('.log-complete', () => true).catch(() => false);
+                    if (isSyncDone) {
+                        resolve();
+                    } else {
+                        setTimeout(tick, 1000);
+                    }
+                }
+                tick();
+            });
+        }
 
-        // TODO add some events via FastComments (add comments, remove some, update some, add some votes, undo those votes), and then see that they get synced locally.
+        await syncToFastComments();
+
+        // pull our testing events, and then see that they get synced locally.
+        async function syncFromFastComments() {
+            return new Promise((resolve) => {
+                async function tick() {
+                    await myApp.cron();
+                    const isSyncDone = Object.keys(myApp.fastComments.commentDB).length > 2;
+                    if (isSyncDone) {
+                        resolve();
+                    } else {
+                        setTimeout(tick, 1000);
+                    }
+                }
+                tick();
+            });
+        }
+
+        await syncFromFastComments();
+
     } catch (e) {
         console.error(e);
     }
 
-    // delete our testing tenant
+    // delete our testing tenant via hitting safe endpoint via puppeteer
+
+
     // TODO have tenant deletion delete integration related stuff
 })();
