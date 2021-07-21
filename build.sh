612e2730674514824cbaabd1f91da38b13495d7c
winrid <winrid@gmail.com>
2021-07-21 08:58:04 -0700

    fixing build script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 cd core/js/example && npm install
 cd ../../..
-cd core/tests && npm install
-cd ../../.. && node core/tests/run-tests-all-projects.js
+cd core/tests && npm install && npm run run-tests-all-projects
